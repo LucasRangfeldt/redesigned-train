1bf666ae9841760e7cf1f884319f1b4c5143d4e8
LucasRangfeldt <lucas_rangfeldt@hotmail.com>
2023-12-18 11:36:07 +0100

    made a script to create a section for each and every category

---
+++ assets/images/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const categories = ['Hardware', 'Software', 'Gaming', 'Mobile', 'Internet', 'Security', 'Social Media', 'Programming', 'Setups and Looks', 'Other'];
+
+    const main = document.querySelector('.main');
+
+    category.forEach(category => {
+        const section = document.createElement('section');
+        section.classList.add('main-section');
+
+        const h2 = document.createElement('h2');
+        h2.textContent = category;
+
+        section.appendChild(h2);
+        main.appendChild(section);
+    });
+
+});
