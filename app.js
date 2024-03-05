6f17ba01ee90e0d552cc0371b3b10dad034f2241
LucasRangfeldt <lucas_rangfeldt@hotmail.com>
2024-03-05 18:37:10 +0100

    fixed typo line 7 in app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express  require('express');
+const express = require('express');
 const routes = require('./routes.js');
 
 const app = express();
 
 app.use(express.json());
-app.use('(', routes);
+app.use('/', routes);
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
